ebd9883e3b67e75daa933c5b32524f32de01e6de
Jadurani Davalos <jadurani.davalos@gmail.com>
2014-05-26 07:20:25 +0000

    Done Chap4 Exercise

--- rango/views.py
+++ rango/views.py
@@ -18,5 +18,7 @@ def index(request):
 	return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
-	return HttpResponse("Rango says: this the fcking about page <a href='/rango/'>Index</a>")
+	context = RequestContext(request)
+	context_dict = {'boldmessage' : "this the fcking about page"}
+	return render_to_response('rango/about.html', context_dict, context)
 
